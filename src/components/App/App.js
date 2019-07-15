916328a1ae692ed78bd6abfbb87ce5c87f210149
Generosa Litton <glitton@newrelic.com>
2019-07-15 20:07:23 +0900

    added component dir

---
+++ src/components/App/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Spotify<span class="highlight">Jamm</span>s</h1>
+        <div class="App">
+          <!-- Add a SearchBar component -->
+          <div class="App-playlist">
+            <!-- Add a SearchResults component -->
+            <!-- Add a Playlist component -->
+          </div>
+        </div>
+      </div>      
+    )
+  }
+}
+
+export default App;
+
