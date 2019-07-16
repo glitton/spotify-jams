e6aee2c6f25ab5b11f1c390fb20420bf8756d960
Generosa Litton <glitton@newrelic.com>
2019-07-16 10:14:59 -0700

    added Tracklist components

--- src/components/Playlist/Playlist.js
+++ src/components/Playlist/Playlist.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./Playlist.css";
+import TrackList from "../TrackList/TrackList";
 
 class Playlist extends React.Component {
   render() {
     return (
       <div class="Playlist">
         <input value="New Playlist" />
-        {/* <!-- Add a TrackList component --> */}
+        <TrackList />
         <button class="Playlist-save">SAVE TO SPOTIFY</button>
       </div>
     );
---
+++ src/components/TrackList/TrackList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./TrackList.css";
+
+const TrackList = props => {
+  return (
+    <div className="TrackList">
+      {/* <!-- You will add a map method that renders a set of Track components  --> */}
+    </div>
+  );
+};
+
+export default TrackList;
